7650ef5d18642e451eeb3c757286401beef4a524
firenza <masudtxg@gmail.com>
2026-04-10 20:38:44 +0700

    extra patch susfs
    using script from https://github.com/JackA1ltman/NonGKI_Kernel_Build_2nd

--- kernel/sys.c
+++ kernel/sys.c
@@ -1194,20 +1194,25 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+extern void susfs_spoof_uname(struct new_utsname* tmp);
+#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
+    susfs_spoof_uname(&tmp);
+#else
     if (!strncmp(current->comm, "bpfloader", 9) ||
-	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4) ||
-	    !strncmp(current->comm, "uprobestats", 11)) {
-		strcpy(tmp.release, "5.10.246");
-		pr_debug("fake uname: %s release=%s\n",
-			 current->comm, tmp.release);
-	}
+        !strncmp(current->comm, "netbpfload", 10) ||
+        !strncmp(current->comm, "netd", 4) ||
+        !strncmp(current->comm, "uprobestats", 11)) {
+        strcpy(tmp.release, "5.10.246");
+    }
+#endif
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;

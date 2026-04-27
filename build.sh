188952bd9396dd276e06c150f4f13ee8c8b96f0b
firenza <masudtxg@gmail.com>
2026-04-27 20:50:40 +0700

    wireguard

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ DATE=$(date '+%Y%m%d-%H%M')
 # Device
 DEVICE="${1:-agate}"
 DEFCONFIG="${DEVICE}_defconfig"
-ZIPNAME="HydrogenKernel-${DEVICE}-${DATE}.zip"
+ZIPNAME="Scuderia-MTK-${DEVICE}-${DATE}.zip"
 
 echo -e "Building for: $DEVICE\n"
 
@@ -19,10 +19,10 @@ echo -e "Building for: $DEVICE\n"
 TC_DIR="$HOME/toolchains/proton-clang"
 CURRENT_DIR=$(pwd)
 if [ ! -d "$TC_DIR" ]; then
-    mkdir -p "$HOME/toolchains"
-    cd "$HOME/toolchains"
-    git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang.git -b clang-15 proton-clang
-    cd "$CURRENT_DIR"
+  mkdir -p "$HOME/toolchains"
+  cd "$HOME/toolchains"
+  git clone --depth=1 https://gitlab.com/LeCmnGend/proton-clang.git -b clang-15 proton-clang
+  cd "$CURRENT_DIR"
 fi
 export PATH="$TC_DIR/bin:$PATH"
 
@@ -30,12 +30,13 @@ export PATH="$TC_DIR/bin:$PATH"
 CLEAN_BUILD=false
 INCLUDE_KSU=false
 for arg in "$@"; do
-    case $arg in
-        -c) CLEAN_BUILD=true ;;
-        -ksu) INCLUDE_KSU=true
-             ZIPNAME="HydrogenKernel-KSU-${DEVICE}-${DATE}.zip"
-             ;;
-    esac
+  case $arg in
+  -c) CLEAN_BUILD=true ;;
+  -ksu)
+    INCLUDE_KSU=true
+    ZIPNAME="HydrogenKernel-KSU-${DEVICE}-${DATE}.zip"
+    ;;
+  esac
 done
 
 [ "$CLEAN_BUILD" = true ] && rm -rf out
@@ -43,22 +44,22 @@ done
 
 # Compilation process
 mkdir -p out
-make O=out ARCH=arm64 $DEFCONFIG
+make O=out ARCH=arm64 CC="ccache clang" LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- $DEFCONFIG
 
 echo -e "\nStarting compilation...\n"
 if make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz; then
-    echo -e "\nKernel compiled successfully! Zipping up...\n"
-    git clone -q --depth=1 https://github.com/rio004/AnyKernel3 AnyKernel3
-    cp out/arch/arm64/boot/Image.gz AnyKernel3
-    rm -rf *zip out/arch/arm64/boot
-    (cd AnyKernel3 && zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder)
-    rm -rf AnyKernel3
-    if [ "$INCLUDE_KSU" = true ]; then
-        git restore drivers/{Makefile,Kconfig}
-        rm -rf KernelSU drivers/kernelsu
-    fi
-    echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
-    echo "Zip: $ZIPNAME"
+  echo -e "\nKernel compiled successfully! Zipping up...\n"
+  git clone -q --depth=1 https://github.com/rio004/AnyKernel3 AnyKernel3
+  cp out/arch/arm64/boot/Image.gz AnyKernel3
+  rm -rf *zip out/arch/arm64/boot
+  (cd AnyKernel3 && zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder)
+  rm -rf AnyKernel3
+  if [ "$INCLUDE_KSU" = true ]; then
+    git restore drivers/{Makefile,Kconfig}
+    rm -rf KernelSU drivers/kernelsu
+  fi
+  echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
+  echo "Zip: $ZIPNAME"
 else
-    echo -e "\nCompilation failed!"
+  echo -e "\nCompilation failed!"
 fi

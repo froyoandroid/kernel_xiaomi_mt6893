3730f79d1aa86c9dcc50c602923ccfd8b6074a0d
firenza <masudtxg@gmail.com>
2026-05-03 03:24:36 +0700

    chore: rename build artifact, remove KernelSU support, and update AnyKernel3 source repository

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ DATE=$(date '+%Y%m%d-%H%M')
 # Device
 DEVICE="${1:-agate}"
 DEFCONFIG="${DEVICE}_defconfig"
-ZIPNAME="Exhorizon-${DEVICE}-${DATE}.zip"
+ZIPNAME="Saving-Private-Kernel-${DEVICE}-${DATE}.zip"
 
 echo -e "Building for: $DEVICE\n"
 
@@ -28,19 +28,13 @@ export PATH="$TC_DIR/bin:$PATH"
 
 # Process options
 CLEAN_BUILD=false
-INCLUDE_KSU=false
 for arg in "$@"; do
   case $arg in
   -c) CLEAN_BUILD=true ;;
-  -ksu)
-    INCLUDE_KSU=true
-    ZIPNAME="HydrogenKernel-KSU-${DEVICE}-${DATE}.zip"
-    ;;
   esac
 done
 
 [ "$CLEAN_BUILD" = true ] && rm -rf out
-[ "$INCLUDE_KSU" = true ] && echo "Save your stuff!!" && curl -LSs "https://raw.githubusercontent.com/tiann/KernelSU/main/kernel/setup.sh" | bash -s v0.9.5
 
 # Compilation process
 mkdir -p out
@@ -49,15 +43,11 @@ make O=out ARCH=arm64 CC="ccache clang" LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-
 echo -e "\nStarting compilation...\n"
 if make -j$(nproc --all) O=out ARCH=arm64 CC="ccache clang" LLVM=1 LLVM_IAS=1 CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz; then
   echo -e "\nKernel compiled successfully! Zipping up...\n"
-  git clone -q --depth=1 https://github.com/rio004/AnyKernel3 AnyKernel3
+  git clone -q --depth=1 https://github.com/froyoandroid/AnyKernel3 AnyKernel3
   cp out/arch/arm64/boot/Image.gz AnyKernel3
   rm -rf *zip out/arch/arm64/boot
   (cd AnyKernel3 && zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder)
   rm -rf AnyKernel3
-  if [ "$INCLUDE_KSU" = true ]; then
-    git restore drivers/{Makefile,Kconfig}
-    rm -rf KernelSU drivers/kernelsu
-  fi
   echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s)!"
   echo "Zip: $ZIPNAME"
 else

17829f095a5093928ad87a674c7da826bce078ca
firenza <masudtxg@gmail.com>
2026-05-03 04:46:26 +0700

    revert: restore thermal target and schedtune boost hold duration Restore CLATM configuration 2 target junction temperature to 65°C and reset SCHEDTUNE_BOOST_HOLD_NS to 50ms. The previous reductions (62°C and 20ms) caused excessive thermal throttling and poor scheduler residency, leading to a significant drop in Geekbench scores.

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -15,7 +15,7 @@ bool schedtune_initialized = false;
 extern struct reciprocal_value schedtune_spc_rdiv;
 
 /* We hold schedtune boost in effect for at least this long */
-#define SCHEDTUNE_BOOST_HOLD_NS 20000000ULL
+#define SCHEDTUNE_BOOST_HOLD_NS 50000000ULL
 
 /*
  * EAS scheduler tunables for task groups.
